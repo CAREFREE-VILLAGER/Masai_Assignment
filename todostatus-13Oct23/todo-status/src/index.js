2b6a7a275936fccfaef5699a720248b64fc9c453
CAREFREE-VILLAGER <83590856+CAREFREE-VILLAGER@users.noreply.github.com>
2023-10-24 17:40:41 +0530

    Update index.js

--- todostatus-13Oct23/todo-status/src/index.js
+++ todostatus-13Oct23/todo-status/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Import the App component
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App /> {/* Render the App component */}
+    <App /> {}
   </React.StrictMode>,
   document.getElementById('root')
 );
